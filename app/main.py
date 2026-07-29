4df0d75bc309178c46a99856f5ec336994498d01
ramsparames <ramsparames12@gmail.com>
2026-07-29 17:22:28 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI(title="NourisHer Backend")
-
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+class SnapshotSubmission(BaseModel):
+    name: str
+    email: str
+    answers: dict[str, int]
+    
 @app.get("/")
 def root():
     return {
