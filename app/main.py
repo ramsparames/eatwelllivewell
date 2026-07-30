05634f139e60fcab49cca8f4787f5b2aeba325b9
ramsparames <ramsparames12@gmail.com>
2026-07-30 20:49:46 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ from pathlib import Path
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from app.database import create_database, save_snapshot
-from app.scoring import calculate_score
+from database import create_database, save_snapshot
+from scoring import calculate_score
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi import HTTPException
 from fastapi.templating import Jinja2Templates
-from app.database import get_all_leads
-from app.database import get_all_leads, get_lead_by_id
+from database import get_all_leads
+from database import get_all_leads, get_lead_by_id
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 app = FastAPI()
